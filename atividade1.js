85605004984aeb451e35333a2ef6d95b81dd93f1
MarianeCaldeira <87836763+MarianeCaldeira@users.noreply.github.com>
2021-09-22 21:13:30 -0300

    Add files via upload

---
+++ atividade1.js
@@ -0,0 +1,40 @@
+function questao1(){
+  var numero_inteiro = 10;
+console.log(numero_inteiro);
+}
+
+function questao2(){
+  var texto_aspas_duplas = "texto";
+  console.log(texto_aspas_duplas);
+}
+
+function questao3(){
+var verdade = true;
+var mentira = false;
+console.log("você está fazendo atividade?");
+console.log(verdade);
+console.log("Você não sabe fazer a atividade?");
+console.log(mentira);
+}
+
+function questao4(){
+var lanche = ["hamburguer","coca-cola", "batata-frita"];
+console.log(lanche)
+}
+
+function questao5(){
+var carro = {cor: "verde", modelo: "Etios", ano: 2021, Seminovo: false};
+console.log(carro);
+console.log(carro.ano);
+console.log(carro.modelo);
+}
+
+function questao6(){
+var valor_nao_definido;
+console.log("valor_nao_definido: " + valor_nao_definido);
+}
+
+function questao7(){
+var valor_vazio = null;
+console.log("valor_vazio: " + valor_vazio);
+}
---
+++ atividade2.js
@@ -0,0 +1,15 @@
+function questao1(){
+    var nome = "Mariane";
+    console.log(nome);
+  }
+  
+  function questao2(){
+    var numero = 18;
+    let idade = numero;
+    console.log(idade);
+  }
+  
+  function questao3(){
+    const PI = 3.14159;
+    console.log(PI);
+  }
